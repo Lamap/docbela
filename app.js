6d1ec800907bfb7e9de912df55f40e2d87629056
SWDEV\akos.lenkei <akos.lenkei@solarwinds.com>
2016-12-28 13:55:56 +0100

    initial

---
+++ app.js
@@ -0,0 +1,13 @@
+
+var express = require('express');
+var app = express();
+var PORT = 3333;
+var request = require('request');
+
+app.get('/', function (req, res) {
+    res.send('Bélák Jenője, Jenők Gézája, Gézák Bélája, Bélák doksija!');
+});
+
+app.listen(PORT, function () {
+    console.log('App is runing on port ' + PORT);
+});
